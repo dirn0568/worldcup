55e016fabdd07f9e591abf40292e76066d35850e
박관우 <dirn0568@naver.com>
2021-07-16 18:00:24 +0900

    subscribe_system2

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from subscribeapp.views import SubscriptionView
+from subscribeapp.views import SubscriptionView, SubscribeListView
 
 app_name = 'subscribeapp'
 
 urlpatterns = [
     path('subscription/', SubscriptionView.as_view(), name='subscription'),
+    path('subscribe_list', SubscribeListView.as_view(), name='subscribe_list'),
 ]
--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -1,9 +1,13 @@
+
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView, FormView, DetailView
+from django.views.generic.list import MultipleObjectMixin
 
+from articleapp.forms import ArticleCreateForm
+from articleapp.models import ArticleCreateModel
 from projectapp.models import ProjectCreateModel
 from subscribeapp.models import SubscribeCreateModel
 
@@ -28,4 +32,19 @@ class SubscriptionView(RedirectView):
         return super(SubscriptionView, self).get(request, *args, **kwargs)
 
 
+# 희안하게 이건 에러가 있음 왜 있는지 모르겠음 바로 아래 문장(필요없는 문장임)#
+# class SubscribeListView(ListView):
+#     model = SubscribeCreateModel
+#     context_object_name = 'article_subscribe'
+#     template_name = 'subscribe_list.html'
+
+class SubscribeListView(ListView):
+    model = ArticleCreateModel
+    context_object_name = 'article_subscribe'
+    template_name = 'subscribe_list.html'
+
+    def get_queryset(self):
+        projects = SubscribeCreateModel.objects.filter(subscribe_user=self.request.user).values_list('subscribe_project')
+        article_subscribe = ArticleCreateModel.objects.filter(article_project__in=projects)
+        return article_subscribe
 
