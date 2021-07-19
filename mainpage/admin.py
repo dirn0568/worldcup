36ac8b7dbba38c23fc0945d57e254115ebe1cb76
박관우 <dirn0568@naver.com>
2021-07-19 20:37:37 +0900

    finish_template3

--- mainpage/admin.py
+++ mainpage/admin.py
@@ -2,9 +2,3 @@ from django.contrib import admin
 
 # Register your models here.
 from mainpage import forms
-
-
-class SearchAdmin(admin.ModelAdmin):
-    list_display = (u'preview', u'caption')
-    search_fields = (u'caption')
-    form = forms.PostDetailForm
--- mainpage/forms.py
+++ mainpage/forms.py
@@ -1,10 +1,19 @@
 from django import forms
 
 class PostSearchForm(forms.Form):
-    search_word = forms.CharField(label='빠른 검색')
+    search_word = forms.CharField(label='빠른 검색',
+                                  widget=forms.TextInput(attrs={'autofocus': 'autofocus',
+                                                                'autocomplete': 'off',
+                                                                'size': '40',
+                                                                'style': 'font-size: x-large',
+                                                                }))
 
-class PostDetailForm(forms.ModelForm):
-    class Meta:
-        widgets = {'caption': forms.TextInput(attrs={'size': 80})}
+class PostDetailForm(forms.Form):
+    search_word = forms.CharField(label='빠른 검색',
+                                  widget=forms.TextInput(attrs={'autofocus': 'autofocus',
+                                                                'autocomplete': 'off',
+                                                                'size': '40',
+                                                                'style': 'font-size: x-large',
+                                                                }))
 
 
