66a5519315d175f29f3d2c4c3541f4d4e6e2d5bb
박관우 <dirn0568@naver.com>
2021-07-27 16:52:39 +0900

    alpha_test

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,4 +1,6 @@
 from django.contrib import messages
+from django.core.exceptions import ValidationError
+from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
@@ -10,6 +12,17 @@ from articleapp.models import ArticleCreateModel
 from likeapp.models import LikeRecordModel
 
 
+@transaction.atomic
+def db_transaction(user, article):
+
+    if LikeRecordModel.objects.filter(user=user, article=article).exists():
+        print('실행중?333333333333333333')
+        raise ValidationError('Like already exists')
+    else:
+        LikeRecordModel(user=user, article=article).save()
+    article.like += 1
+    article.save()
+
 class LikeArticleView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
         print('실행중?11111111111111111')
@@ -20,16 +33,11 @@ class LikeArticleView(RedirectView):
         user = self.request.user
         article = get_object_or_404(ArticleCreateModel, pk=kwargs['pk'])
 
-        if LikeRecordModel.objects.filter(user=user, article=article).exists():
-            print('실행중?333333333333333333')
+        try:
+            db_transaction(user, article)
+            messages.add_message(self.request, messages.SUCCESS, '좋아요가 반영되었습니다.')
+        except ValidationError:
             messages.add_message(self.request, messages.ERROR, '좋아요는 한번만 가능합니다.')
             return HttpResponseRedirect(reverse('articleapp:article_detail', kwargs={'pk': kwargs['pk']}))
-        else:
-            LikeRecordModel(user=user, article=article).save()
-
-        article.like += 1
-        article.save()
-
-        messages.add_message(self.request, messages.SUCCESS, '좋아요가 반영되었습니다.')
 
         return super(LikeArticleView, self).get(self.request, *args, **kwargs)
