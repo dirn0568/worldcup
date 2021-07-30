919f184f09d0352c4b73ba9cd94f5cc10091b4c7
박관우 <dirn0568@naver.com>
2021-07-30 17:04:39 +0900

    add_tetris

--- static/js/tetris.js
+++ static/js/tetris.js
@@ -14,9 +14,9 @@ let tempMovingItem;
 const BLOCKS = {
     tree: [
         [[2, 1], [0, 1], [1, 0], [1, 1]],
-        [],
-        [],
-        [],
+        [[1, 2], [0, 1], [1, 0], [1, 1]],
+        [[1, 2], [0, 1], [2, 1], [1, 1]],
+        [[2, 1], [1, 2], [1, 0], [1, 1]],
     ]
 }
 
@@ -53,45 +53,75 @@ function prependNewLine() {
 }
 
 // const { type, direction, top, left } = tempMovingItem; tempMovingItem의 데이터가 알아서 저 리스트 안에 들어가는게 가능?
-// 삼학연산자 내일 다시보기
-// 유튜브 39분 47초까지의 상황
-function renderBlocks() {
+// 삼학연산자 내일 다시보기(삼학연산자가 없어도 아마될꺼 같음 왜냐하면 없어도 None타입 데이터가 나오기때문에)
+// html 에서 <ul> enter </ul> 이런식으로 저장되면 enter부분을 text로 인식되어서 top으로 내릴때 두번내려야했던거임 <ul></ul> 이렇게 바로 저장해야함
+// 유튜브 50분 40초까지의 상황
+function renderBlocks(moveType="") {
     const { type, direction, top, left } = tempMovingItem;
     const movingBlocks = document.querySelectorAll(".moving");
     movingBlocks.forEach(moving => {
         moving.classList.remove(type, "moving");
     })
-    BLOCKS[type][direction].forEach(block => {
+    BLOCKS[type][direction].some(block => {
         const x = block[0] + left;
         const y = block[1] + top;
+//        console.log(playground.childNodes[y])
         const target = playground.childNodes[y] ? playground.childNodes[y].childNodes[0].childNodes[x] : null;
-        console.log(target, '@@@@@@@@@@@@@@')
+        console.log(target)
         const isAvailable = checkEmpty(target);
         if (isAvailable) {
             target.classList.add(type, "moving")
         } else {
-//            console.log('stop')
+            console.log('stop')
             tempMovingItem = { ...movingItem }
             setTimeout(() => {
+//                console.log('stop', "####################")
                 renderBlocks();
+//                console.log('stop2', "####################")
+                if (moveType === "top") {
+                    seizeBlock();
+                }
             }, 0)
-
+            return true;
         }
     })
+//    console.log('play', "################################")
     movingItem.left = left;
     movingItem.top = top;
     movingItem.direction = direction;
 }
 function checkEmpty(target){
-    if(!target){
+    if(!target || target.classList.contains("seized")){
         return false;
     }
     return true;
 }
 
+function seizeBlock(){
+    const movingBlocks = document.querySelectorAll(".moving");
+    movingBlocks.forEach(moving => {
+        moving.classList.remove("moving");
+        moving.classList.add("seized");
+    })
+    generateNewBlock()
+}
+
+function generateNewBlock(){
+    movingItem.top = 0;
+    movingItem.left = 3;
+    movingItem.direction = 0;
+    tempMovingItem = { ...movingItem };
+    renderBlocks()
+}
 
 function moveBlock(moveType, amount){
     tempMovingItem[moveType] += amount
+    renderBlocks(moveType)
+}
+
+function changeDirection(){
+    const direction = tempMovingItem.direction;
+    direction === 3 ? tempMovingItem.direction = 0 : tempMovingItem.direction += 1;
     renderBlocks()
 }
 
@@ -106,6 +136,9 @@ document.addEventListener("keydown", e => {
             break;
         case 40:
             moveBlock("top", 1);
+            break;
+        case 32:
+            changeDirection();
             break;
         default:
             break;
