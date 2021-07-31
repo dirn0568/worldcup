459ddf967108ca582584bd0d8f777156cd5c5a5b
박관우 <dirn0568@naver.com>
2021-07-31 13:13:44 +0900

    tetris_finish

--- static/js/tetris.js
+++ static/js/tetris.js
@@ -1,6 +1,10 @@
+import BLOCKS from "./blocks.js"
+
 //DOM
 const playground = document.querySelector(".playground > ul");
-
+const gameText = document.querySelector(".game-text");
+const scoreDisplay = document.querySelector(".score");
+const restartButton = document.querySelector(".game-text > button");
 // Setting
 const GAME_ROWS = 20;
 const GAME_COLS = 10;
@@ -11,17 +15,8 @@ let duration = 500;
 let downInterval;
 let tempMovingItem;
 
-const BLOCKS = {
-    tree: [
-        [[2, 1], [0, 1], [1, 0], [1, 1]],
-        [[1, 2], [0, 1], [1, 0], [1, 1]],
-        [[1, 2], [0, 1], [2, 1], [1, 1]],
-        [[2, 1], [1, 2], [1, 0], [1, 1]],
-    ]
-}
-
 const movingItem = {
-    type: "tree",
+    type: "",
     direction: 0,
     top: 4,
     left: 3,
@@ -29,6 +24,7 @@ const movingItem = {
 
 
 init()
+console.log(playground)
 
 // functions
 // { ...movingItem } 이해가 잘안됨 내일 다시한번 봐볼껏
@@ -36,9 +32,9 @@ function init(){
     tempMovingItem = { ...movingItem };
 
     for (let i = 0; i < GAME_ROWS; i++) {
-    prependNewLine()
+        prependNewLine()
     }
-    renderBlocks()
+    generateNewBlock()
 }
 
 function prependNewLine() {
@@ -55,7 +51,7 @@ function prependNewLine() {
 // const { type, direction, top, left } = tempMovingItem; tempMovingItem의 데이터가 알아서 저 리스트 안에 들어가는게 가능?
 // 삼학연산자 내일 다시보기(삼학연산자가 없어도 아마될꺼 같음 왜냐하면 없어도 None타입 데이터가 나오기때문에)
 // html 에서 <ul> enter </ul> 이런식으로 저장되면 enter부분을 text로 인식되어서 top으로 내릴때 두번내려야했던거임 <ul></ul> 이렇게 바로 저장해야함
-// 유튜브 50분 40초까지의 상황
+// 유튜브 1시간 10분 17초까지의 상황
 function renderBlocks(moveType="") {
     const { type, direction, top, left } = tempMovingItem;
     const movingBlocks = document.querySelectorAll(".moving");
@@ -65,19 +61,18 @@ function renderBlocks(moveType="") {
     BLOCKS[type][direction].some(block => {
         const x = block[0] + left;
         const y = block[1] + top;
-//        console.log(playground.childNodes[y])
         const target = playground.childNodes[y] ? playground.childNodes[y].childNodes[0].childNodes[x] : null;
-        console.log(target)
         const isAvailable = checkEmpty(target);
         if (isAvailable) {
             target.classList.add(type, "moving")
         } else {
-            console.log('stop')
             tempMovingItem = { ...movingItem }
+            if(moveType === 'retry'){
+                clearInterval(downInterval)
+                showGameoverText()
+            }
             setTimeout(() => {
-//                console.log('stop', "####################")
-                renderBlocks();
-//                console.log('stop2', "####################")
+                renderBlocks('retry');
                 if (moveType === "top") {
                     seizeBlock();
                 }
@@ -103,10 +98,43 @@ function seizeBlock(){
         moving.classList.remove("moving");
         moving.classList.add("seized");
     })
+    checkMatch()
+}
+
+function checkMatch(){
+    const childNodes = playground.childNodes;
+    childNodes.forEach(child => {
+        let matched = true;
+        child.children[0].childNodes.forEach(li => {
+            if(!li.classList.contains("seized")){
+                matched = false;
+            }
+        })
+        if(matched){
+            child.remove();
+            prependNewLine()
+            score += 100;
+            console.log(score)
+            scoreDisplay.innerText = score;
+            if (score % 500 == 0){
+                duration -= 50;
+            }
+        }
+    })
     generateNewBlock()
 }
 
 function generateNewBlock(){
+
+    clearInterval(downInterval);
+    downInterval = setInterval(()=>{
+        moveBlock('top', 1)
+    }, duration)
+
+    const blockArray = Object.entries(BLOCKS);
+    const randomIndex = Math.floor(Math.random() * blockArray.length)
+
+    movingItem.type = blockArray[randomIndex][0]
     movingItem.top = 0;
     movingItem.left = 3;
     movingItem.direction = 0;
@@ -125,6 +153,17 @@ function changeDirection(){
     renderBlocks()
 }
 
+function dropBlock(){
+    clearInterval(downInterval);
+    downInterval = setInterval(() => {
+        moveBlock("top", 1)
+    }, 10)
+}
+
+function showGameoverText(){
+    gameText.style.display = "flex"
+}
+
 // event handling
 document.addEventListener("keydown", e => {
     switch(e.keyCode){
@@ -140,7 +179,14 @@ document.addEventListener("keydown", e => {
         case 32:
             changeDirection();
             break;
+        case 38:
+            dropBlock();
+            break;
         default:
             break;
     }
 })
+
+//function test() {
+//    console.log(setInterval( "alert('good')", 1000))
+//}
