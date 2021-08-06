62edfa6ed3aa83b7da50b17613589be9209035ac
박관우 <dirn0568@naver.com>
2021-08-06 20:47:13 +0900

    finish_worldcup

--- static/js/worldcup.js
+++ static/js/worldcup.js
@@ -1,12 +1,12 @@
 var picture_array = new Array();
-picture_array[0] = "/static/001.webm";
-picture_array[1] = "/static/002.webm";
-picture_array[2] = "/static/003.webm";
-picture_array[3] = "/static/004.webm";
-picture_array[4] = "/static/005.mp4";
-picture_array[5] = "/static/006.mp4";
-picture_array[6] = "/static/007.mp4";
-picture_array[7] = "/static/008.mp4";
+picture_array[0] = "/static/staticfiles/001.webm";
+picture_array[1] = "/static/staticfiles/002.webm";
+picture_array[2] = "/static/staticfiles/003.webm";
+picture_array[3] = "/static/staticfiles/004.webm";
+picture_array[4] = "/static/staticfiles/005.mp4";
+picture_array[5] = "/static/staticfiles/006.mp4";
+picture_array[6] = "/static/staticfiles/007.mp4";
+picture_array[7] = "/static/staticfiles/008.mp4";
 
 
 var randomIndex = 0
